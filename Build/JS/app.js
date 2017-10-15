83c8d0c14be1c281c73e3a78731aad410bfab83e
javpet <javorkai.peter@gmail.com>
2017-10-15 17:32:18 +0200

    fixing instafeed issues

--- Build/JS/app.js
+++ Build/JS/app.js
@@ -1,6 +1,7 @@
 var feed = new Instafeed({
 	get: "tagged",
 	tagName: "awesome",
-	clientId: "3d73383c990a4e7da1cca60a267737a4"
+	clientId: "3d73383c990a4e7da1cca60a267737a4",
+	accessToken: "1423139674.1677ed0.86cc23591ff84ea8a8f13fdabe52478a"
 });
 feed.run();
